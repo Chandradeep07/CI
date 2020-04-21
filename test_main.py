388dac300ace914ca77ea57b134416b9b3a887d0
Chandradeep07 <60056765+Chandradeep07@users.noreply.github.com>
2020-04-21 20:28:13 +0530

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,7 +1,7 @@
 import main
 def test_multiply():
-    result=main.multiply(3 , 4)
-    result=_2=main.multiply(2 , 3)
+    result=main.multiply(3, 4)
+    result=_2=main.multiply(2, 3)
     print('running tests...')
     assert result_2 == 4
     assert result == 12
